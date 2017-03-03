54f05594e86f31a11482a79de77cf5467864eb96
oacell <oacell@localhost.localdomain>
2017-03-03 15:31:00 +0530

    fixed the issue on routing problem in firefox

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'welcome/index'
 
-
+ 
  devise_for :users, controllers: {
       sessions: 'users/sessions',
       passwords: 'users/passwords',

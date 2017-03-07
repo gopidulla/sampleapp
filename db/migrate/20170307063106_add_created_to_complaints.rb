ea3783cb9c0bd4bf70e13ff0998a3f62ccbaffac
gopidulla <khajamainoddin90@gmail.com>
2017-03-07 22:25:47 +0530

    removed created_time and updated_time fields

--- db/migrate/20170307063106_add_created_to_complaints.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatedToComplaints < ActiveRecord::Migration[5.0]
-  def change
-    add_column :complaints, :created_time, :datetime
-  end
-end
--- db/migrate/20170307063436_add_updated_to_complaints.rb
+++
@@ -1,5 +0,0 @@
-class AddUpdatedToComplaints < ActiveRecord::Migration[5.0]
-  def change
-    add_column :complaints, :updated_time, :datetime
-  end
-end

500f6fa2a6b742c29fd5cd70535c59b9db969102
khajamainoddin <khajamainoddin.mohd@gmail.com>
2017-03-07 16:46:14 +0530

    added sessions timeout using devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
+         :timeoutable,
          :authentication_keys => [:username]
-
+         
   def email_required?
     false
   end
